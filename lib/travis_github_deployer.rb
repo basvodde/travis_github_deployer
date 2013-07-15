a37c1521f9f8df8dea8e65b86e6975a4bf953479
Bas Vodde <basv@odd-e.com>
2013-07-15 18:21:26 +0800

    Pulled out files from CppUTest repository and made a Gemspec

---
+++ lib/travis_github_deployer.rb
@@ -0,0 +1,3 @@
+
+require 'travis_github_deployer/git_command_line.rb'
+require 'travis_github_deployer/travis_github_deployer.rb'
---
+++ lib/travis_github_deployer/version.rb
@@ -0,0 +1,4 @@
+
+module TravisGithubDeployerVersion
+  VERSION = "0.1.0"
+end
